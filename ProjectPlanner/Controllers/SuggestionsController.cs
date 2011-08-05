024c18233af023190e525289cee04bc361ad379c
João Silvestre <joaopaulosilvestre@hotmail.com>
2011-08-05 19:49:02 +0100

    Started some views.
    Continuation of the development of the suggestions controller.

--- ProjectPlanner/Controllers/SuggestionsController.cs
+++ ProjectPlanner/Controllers/SuggestionsController.cs
@@ -15,12 +15,54 @@ namespace ProjectPlanner.Controllers
 
         public ActionResult Index()
         {
-            return View();
-        }
+            ProjectPlannerContext ctx = new ProjectPlannerContext();
 
+            return View(ctx.Users.Select(p => p.Suggestions));
+        }
+        
 
         //
-        // GET: /Suggestions/Add
+        // GET: /Suggestions/
+
+        public ActionResult Index(string name)
+        {
+            ProjectPlannerContext ctx = new ProjectPlannerContext();
+
+            string username = ModelState.IsValid ? name : User.Identity.Name;
+
+            return View(ctx.Users.Single(u => u.Username == username).Suggestions);
+        }
+
+        //
+        // GET: /Suggestions/SupportedSuggestions
+
+        public ActionResult SupportedSuggestions(string name)
+        {
+            ProjectPlannerContext ctx = new ProjectPlannerContext();
+
+            string username = ModelState.IsValid ? name : User.Identity.Name;
+
+            return View(ctx.Users.Single(u => u.Username == username).SupportedSuggestions);
+        }
+
+        //
+        // POST: /Suggestions/Support
+
+        [HttpPost]
+        public ActionResult Support(int ideaID)
+        {
+            ProjectPlannerContext ctx = new ProjectPlannerContext();
+
+            Idea idea = ctx.Users.Select(u => u.Suggestions).Single(l => l.Where(s => s.IdeaID == ideaID).Count() != 0).Single(s => s.IdeaID == ideaID);
+
+            ctx.Users.Single(u => u.Username == User.Identity.Name).SupportedSuggestions.Add(idea);
+
+            return Json(true);
+        }
+
+        //
+        // POST: /Suggestions/Add
+        
         [HttpPost]
         public ActionResult Add(IdeaModel model)
         {
@@ -31,13 +73,19 @@ namespace ProjectPlanner.Controllers
                 Idea idea = new Idea(model.Name, model.BriefDescription, model.Description);
 
                 currentUser.Suggestions.Add(idea);
+
+                return Json(idea.IdeaID);
             }
 
             return View();
         }
 
+        
+        //
+        // POST: /Suggestions/Remove
+
         [HttpPost]
-        public ActionResult Remove(int ideaID)
+        public ActionResult Remove(int ideaID, string successUrl)
         {
             if (ModelState.IsValid)
             {
@@ -46,6 +94,8 @@ namespace ProjectPlanner.Controllers
                 Idea idea = currentUser.Suggestions.SingleOrDefault(i => i.IdeaID == ideaID);
 
                 currentUser.Suggestions.Remove(idea);
+
+                return Redirect(successUrl);
             }
 
             return View();
